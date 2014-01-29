350b8558d2691242084981eb582f04db0a9168f5
Ben Lovell <ben.j.lovell@gmail.com>
2014-01-29 03:30:26 +0000

    Working tests for everything, a little more defensive

--- src/cljone/core.clj
+++ src/cljone/core.clj
@@ -2,24 +2,23 @@
   (:require [clojure.string :as string])
   (:import [java.lang Math])) 
 
-(def model (atom {}))
 
 (defn train!
   "Trains the model with the feature associating with the category symbol"
-  [category feature]
+  [category feature model]
   (swap! model update-in [category feature]
          (fnil inc 0)))
 
 (defn category-total
-  ^Number [category]
+  ^Number [category model]
   (reduce + (vals (category @model))))
 
 (defn feature-probability
   "Given a category and feature, probability the feature is in that category."
-  [category feature]
+  [category feature model]
   (let [feature-count (or (get (category @model) feature) 
                           0) ;not in hash-map means 0 probability
-        total (category-total category)]
+        total (category-total category model)]
     (/ feature-count total)))
 
 (defn get-features
@@ -28,21 +27,24 @@
    (string/split text #"\s+"))
 
 (defn text-probability
-  [category text]
+  [category text model]
   (reduce +
-          (map #(let [prob (feature-probability category %)] 
-                  (if (= 0 prob) 
-                    0 
+          (map #(let [prob (feature-probability category % model)] 
+                  (if (or (= 0 prob) (= 1 prob)) 
+                    prob 
                     (- (java.lang.Math/log prob))))
                (get-features text))))
 
-(defn classify [text] 
+(defn classify [text model] 
+  "Classifies a given text with a given model. Returns :nil on unclassified."
   (let [probs (into {}
         (map vector 
              (keys @model)
-             (map #(text-probability % text)
+             (map #(text-probability % text model)
                   (keys @model))))]
-  (apply max-key #(% probs) (keys @model))))
+  (if-not (apply distinct? (vals probs))
+    :nil
+    (apply max-key #(% probs) (keys @model)))))
 
 
-(-> @model)
+;(-> @model)
--- test/cljone/core_test.clj
+++ test/cljone/core_test.clj
@@ -2,6 +2,79 @@
   (:require [clojure.test :refer :all]
             [cljone.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn dotraining [model] 
+  ;a bit eww, perhaps refactor later
+  (do (train! :positive "test" model)
+      (train! :positive "test" model)
+      (train! :negative "bad" model)
+      (train! :negative "terrible" model)))
+
+(defn setup-model [f]
+  (def tmodel (atom {}))
+  (def rmodel (atom {}))
+  (dotraining rmodel)
+  (f))
+
+;(def tmodel (atom {}))
+(use-fixtures :once setup-model)
+
+
+(deftest train-test
+  (testing "Can train basic"
+    (is (= {:positive {"test" 1}}
+           (do (train! :positive "test" tmodel)
+               @tmodel)))
+    (is (= {:positive {"test" 2}}
+            (do (train! :positive "test" tmodel)
+                @tmodel) ))
+    (is (= {:negative {"bad" 1}, 
+            :positive {"test" 2}}
+           (do (train! :negative "bad" tmodel)
+               @tmodel)))
+    (is (= {:negative {"bad" 1 "terrible" 1}
+            :positive {"test" 2} }
+           (do (train! :negative "terrible" tmodel)
+               @tmodel))))
+
+  (testing "Category-total"
+    (is (= 2
+           (category-total :positive tmodel)))
+    (is (= 2
+           (category-total :negative tmodel)))))
+
+(deftest feature-test
+  (testing "Feature probabilities"
+    (is (= 1
+           (feature-probability :positive "test" rmodel)))
+    (is (= 0
+           (feature-probability :positive "nil" rmodel)))
+    (is (= 1/2
+           (feature-probability :negative "bad" rmodel)))
+    (is (= 1/2
+           (feature-probability :negative "terrible" rmodel))))
+  
+  (testing "Getting features"
+    (is (= ["a" "test" "of" "feature" "extraction"]
+           (get-features "a test of feature extraction"))))) 
+
+(deftest text-test
+  (testing "∝ probability of a text being in a category"
+    (is (= 0
+           (text-probability :positive "not in the category" rmodel)))
+    (is (= 1 
+           (text-probability :positive "test" rmodel)))
+    (is (= 0.6931471805599453
+           (text-probability :negative "terrible" rmodel)))
+    (is (= 0.6931471805599453
+           (text-probability :negative "bad" rmodel)))
+    (is (= 1.3862943611198906 
+           (text-probability :negative "terrible bad string" rmodel)))))
+
+(deftest classify-test
+  (testing "Classifications"
+    (is (= :nil
+           (classify "a string that is not in either dictionary" rmodel)))
+    )
+  )
+
+(run-tests)
